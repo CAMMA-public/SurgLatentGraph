a2eabe47f8d16184dda40512b72e4d48b25fdb22
Aditya Murali <aditya.murali@ext.ihu-strasbourg.eu>
2023-04-19 15:43:12 +0200

    added scripts

---
+++ slurms/def_detr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 8
+#SBATCH --qos=qos_gpu-t4
+#SBATCH --gres=gpu:4
+#SBATCH --time=20:00:00
+#SBATCH -p gpu_p2
+#SBATCH -J latentgraph_def_detr
+#SBATCH --error latentgraph_def_detr_error.log
+#SBATCH --output latentgraph_def_detr.log
+#SBATCH -A lbw@v100
+
+module purge
+module load anaconda-py3/2019.03
+module load gcc/9.3.0
+module load cuda/10.2
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib64
+export MMDETECTION=$WORK/mmdet_files
+export PYTHONPATH=${PYTHONPATH}:/gpfswork/rech/lbw/uou65jw/latentgraph
+
+cd $WORK/latentgraph
+source $(conda info --base)/bin/activate
+conda activate camma
+
+./slurms/run_all.sh def_detr
