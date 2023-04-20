5cfccbc411c65eba6e90b12c98657007a7f3d844
Aditya Murali <aditya.murali@ext.ihu-strasbourg.eu>
2023-04-20 14:05:09 +0200

    updated slurms

--- slurms/run_rn_mgpu.sh
+++ slurms/run_rn_mgpu.sh
@@ -3,12 +3,12 @@ dataset='endoscapes'
 
 # RUN MT (WITH/WITHOUT RECON IN PARALLEL)
 
-# train
-export CUDA_VISIBLE_DEVICES=0 && python $MMDETECTION/tools/train.py ${base_cfg_dir}/simple_cvs_classifier.py &
-export CUDA_VISIBLE_DEVICES=1 && python $MMDETECTION/tools/train.py ${base_cfg_dir}/simple_cvs_classifier_with_recon.py &
-wait
+## train
+#export CUDA_VISIBLE_DEVICES=0 && python $MMDETECTION/tools/train.py ${base_cfg_dir}/simple_cvs_classifier.py &
+#export CUDA_VISIBLE_DEVICES=1 && python $MMDETECTION/tools/train.py ${base_cfg_dir}/simple_cvs_classifier_with_recon.py &
+#wait
 
 # test
-export CUDA_VISIBLE_DEVICES=0 && python $MMDETECTION/tools/test.py $(ls work_dirs/simple_cvs_classifier/best_${dataset}* | tail -1) &
-export CUDA_VISIBLE_DEVICES=1 && python $MMDETECTION/tools/test.py $(ls work_dirs/simple_cvs_classifier_with_recon/best_${dataset}* | tail -1) &
+export CUDA_VISIBLE_DEVICES=0 && python $MMDETECTION/tools/test.py ${base_cfg_dir}/simple_cvs_classifier.py $(ls work_dirs/simple_cvs_classifier/best_${dataset}* | tail -1) &
+export CUDA_VISIBLE_DEVICES=1 && python $MMDETECTION/tools/test.py ${base_cfg_dir}/simple_cvs_classifier_with_recon.py $(ls work_dirs/simple_cvs_classifier_with_recon/best_${dataset}* | tail -1) &
 wait
