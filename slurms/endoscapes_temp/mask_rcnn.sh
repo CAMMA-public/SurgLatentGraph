67cae44d52606eb34792102e550fe44c58a37ecb
Adit Murali <adit.murali@outlook.com>
2023-07-17 11:52:58 +0200

    cleaned up cfgs, added slurms

---
+++ slurms/endoscapes_temp/mask_rcnn.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 8
+#SBATCH --qos=qos_gpu-t4
+#SBATCH --gres=gpu:4
+#SBATCH --time=20:00:00
+#SBATCH -p gpu_p13
+#SBATCH -J latentgraph_mask_rcnn
+#SBATCH --error latentgraph_mask_rcnn_error.log
+#SBATCH --output latentgraph_mask_rcnn.log
+#SBATCH -A lbw@v100
+#SBATCH -C v100-32g
+
+module purge
+module load anaconda-py3/2019.03
+module load gcc/9.3.0
+module load cuda/10.2
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib64
+export MMDETECTION=$WORK/mmdet_files
+export PYTHONPATH=${PYTHONPATH}:/gpfswork/rech/lbw/uou65jw/latentgraph
+
+cd $WORK/latentgraph
+source $(conda info --base)/bin/activate
+conda activate camma
+
+./slurms/run_all_temp.sh mask_rcnn
--- temporal_cfgs/sv2lstg_mask_rcnn_10.py
+++ temporal_cfgs/sv2lstg_mask_rcnn_10.py
@@ -90,7 +90,7 @@ test_evaluator = [
 # Running settings
 train_cfg = dict(
     type='EpochBasedTrainLoop',
-    max_epochs=20,
+    max_epochs=10,
     val_interval=1)
 val_cfg = dict(type='ValLoopKeyframeEval')
 test_cfg = dict(type='TestLoopKeyframeEval')
