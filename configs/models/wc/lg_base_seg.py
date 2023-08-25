6f8eb6aba8b6c54144eaece8ad5b646b71dc8f22
Siddhant Satyanaik <satyanaik@unistra.fr>
2023-08-25 15:37:43 +0200

    added wc cfgs

---
+++ configs/models/wc/lg_base_seg.py
@@ -0,0 +1,31 @@
+import os
+
+_base_ = ['lg_base_box.py']
+
+# metric
+val_evaluator = dict(
+    type='CocoMetricRGD',
+    prefix='wc',
+    data_root=_base_.data_root,
+    data_prefix=_base_.val_data_prefix,
+    ann_file=os.path.join(_base_.data_root, 'val/annotation_coco.json'),
+    metric=['bbox', 'segm'],
+    additional_metrics=['reconstruction'],
+    use_pred_boxes_recon=False,
+)
+
+test_evaluator = dict(
+    type='CocoMetricRGD',
+    prefix='wc',
+    data_root=_base_.data_root,
+    data_prefix=_base_.test_data_prefix,
+    ann_file=os.path.join(_base_.data_root, 'test/annotation_coco.json'),
+    metric=['bbox', 'segm'],
+    additional_metrics=['reconstruction'],
+    use_pred_boxes_recon=False,
+    outfile_prefix='./results/wc_preds/test/lg_cvs',
+)
+
+default_hooks = dict(
+    checkpoint=dict(save_best='wc/segm_mAP'),
+)
---
+++ configs/models/wc/simple_cvs_classifier.py
@@ -0,0 +1,115 @@
+import os
+
+# dataset, optimizer, and runtime cfgs
+_base_ = [
+    '../datasets/wc/wc_instance.py',
+    os.path.expandvars('$MMDETECTION/configs/_base_/schedules/schedule_1x.py'),
+    os.path.expandvars('$MMDETECTION/configs/_base_/default_runtime.py')
+]
+
+data_root = _base_.data_root
+val_data_prefix = _base_.val_dataloader.dataset.data_prefix.img
+test_data_prefix = _base_.test_dataloader.dataset.data_prefix.img
+
+orig_imports = _base_.custom_imports.imports
+custom_imports = dict(imports=orig_imports + ['model.simple_cvs_predictor', 'evaluator.CocoMetricRGD'], allow_failed_imports=False)
+
+model = dict(
+    type='SimpleCVSPredictor',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=-1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch',
+        init_cfg=dict(type='Pretrained',
+            checkpoint='torchvision://resnet50')),
+    loss=dict(
+        type='CrossEntropyLoss',
+        use_sigmoid=True,
+        class_weight=[153.7875, 90.46323529, 62.13636364],
+    ),
+    num_classes=3,
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_mask=True,
+        pad_size_divisor=32,
+    ),
+)
+
+# dataset
+train_dataloader = dict(
+    batch_size=32,
+    num_workers=4,
+    dataset=dict(
+        ann_file='train/annotation_ds_coco.json',
+    ),
+)
+val_dataloader = dict(
+    batch_size=32,
+    num_workers=4,
+    dataset=dict(
+        ann_file='val/annotation_ds_coco.json',
+    ),
+)
+test_dataloader = dict(
+    batch_size=32,
+    num_workers=4,
+    dataset=dict(
+        ann_file='test/annotation_ds_coco.json',
+    ),
+)
+
+# evaluators
+val_evaluator = [
+    dict(
+        type='CocoMetricRGD',
+        prefix='wc',
+        data_root=_base_.data_root,
+        data_prefix=_base_.val_dataloader.dataset.data_prefix.img,
+        ann_file=os.path.join(_base_.data_root, 'val/annotation_ds_coco.json'),
+        use_pred_boxes_recon=True,
+        metric=[],
+    )
+]
+
+test_evaluator = [
+    dict(
+        type='CocoMetricRGD',
+        prefix='wc',
+        data_root=_base_.data_root,
+        data_prefix=_base_.test_dataloader.dataset.data_prefix.img,
+        ann_file=os.path.join(_base_.data_root, 'test/annotation_ds_coco.json'),
+        metric=[],
+        #additional_metrics = ['reconstruction'],
+        use_pred_boxes_recon=True,
+        outfile_prefix='./results/wc_preds/test/r50'
+    ),
+]
+
+# optimizer
+del _base_.param_scheduler
+optim_wrapper = dict(
+    _delete_=True,
+    optimizer=dict(type='AdamW', lr=0.00001),
+)
+auto_scale_lr = dict(enable=False)
+
+# Running settings
+train_cfg = dict(
+    type='EpochBasedTrainLoop',
+    max_epochs=20,
+    val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+# hooks
+default_hooks = dict(
+    checkpoint=dict(save_best='wc/ds_average_precision'),
+)
