af6fa53e965c24aac28c1d574a3e0777d63863cc
Adit Murali <adit.murali@outlook.com>
2023-11-21 19:28:07 +0100

    proper epoch scaling for small_wc

--- configs/models/small_wc/lg_base_box.py
+++ configs/models/small_wc/lg_base_box.py
@@ -80,20 +80,20 @@ test_evaluator = dict(
 # learning rate
 param_scheduler = [
     dict(
-        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=200),
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=750),
     dict(
         type='MultiStepLR',
         begin=0,
-        end=40,
+        end=75,
         by_epoch=True,
-        milestones=[16, 32],
+        milestones=[30, 60],
         gamma=0.1)
 ]
 
 # Running settings
 train_cfg = dict(
     type='EpochBasedTrainLoop',
-    max_epochs=40,
+    max_epochs=75,
     val_interval=1)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
