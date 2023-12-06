a2732ac76682fff3c2649e70fa3ba60e39517e54
Adit Murali <adit.murali@outlook.com>
2023-12-06 22:56:42 +0100

    switch temporal to iter based cfg - done for endoscapes, pending for other datasets

--- configs/temporal_models/endoscapes/sv2lstg_10_base.py
+++ configs/temporal_models/endoscapes/sv2lstg_10_base.py
@@ -16,3 +16,17 @@ _base_.sv2lstg_model.clip_size = num_temp_frames
 train_dataloader = dict(batch_size=10)
 val_dataloader = dict(batch_size=10)
 test_dataloader = dict(batch_size=10)
+
+default_hooks = dict(
+    checkpoint=dict(
+        by_epoch=False,
+        interval=175,
+    ),
+)
+
+train_cfg = dict(
+    _delete_=True,
+    type='IterBasedTrainLoop',
+    max_iters=3500,
+    val_interval=175,
+)
